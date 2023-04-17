e4f46c50a162dc030bad2e7fa93210924e91ee01
Filigs <40748182+Filigs@users.noreply.github.com>
2023-04-17 13:47:37 +0100

    slushy changes to slushies

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home() {
     "Wines",
     "Alcoholic Beverages",
     "Cocktails",
-    "Slushy",
+    "Slushies",
     "Acepipe Special",
     "Ice Cream Bowls",
     "Crepes",
