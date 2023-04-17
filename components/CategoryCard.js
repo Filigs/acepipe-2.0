9324a27bd6644e43533f5386b8eb60133fe042bc
Filigs <40748182+Filigs@users.noreply.github.com>
2023-04-17 09:49:35 +0100

    image updates
    * replaced drinks image;
    * added "sizes" prop to Image component to reduce loading times;

--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -13,7 +13,13 @@ function CategoryCard({ title, language }) {
   return (
     <div className={styles.categoryCard}>
       <div className={styles.cardImageContainer}>
-        <Image src={cardImage} alt={title} fill />
+        <Image
+          src={cardImage}
+          alt={title}
+          fill
+          sizes="(max-width: 767px) 90vw, (max-width: 991px) 45vw, 22vw"
+          className={styles.cardImage}
+        />
       </div>
       <div className={styles.categoryCardTitle}>{title}</div>
     </div>
