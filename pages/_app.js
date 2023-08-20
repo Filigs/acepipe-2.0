7d404d639f9ee1e07c0a0d37ed8c2a76e79e794d
Filigs <40748182+Filigs@users.noreply.github.com>
2023-08-20 23:04:08 +0100

    update

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import "@/styles/globals.css";
 import BackToTopButton from "@/utils/scrollButton";
 import "in-viewport";
 import { LanguageProvider } from "../utils/LanguageContext";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }) {
       <Layout>
         <Component {...pageProps} />
         <BackToTopButton />
+        <Analytics />
       </Layout>
     </LanguageProvider>
   );
